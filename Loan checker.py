b6cd064b221ee24b03c5313b9b5b2311b4e1d546
Sazzy_potato <sazzy4255@zohomail.in>
2026-09-22 11:32:26 +0530

    edited the age and the credit score

--- Loan checker.py
+++ Loan checker.py
@@ -11,9 +11,12 @@ while True:
     print("")
 
     age=int(input("Enter your age: "))
-    if age>20:
+    if age>80:
+        print("Congrats on living so long but unfortunatly we have to reject your loan.")
+        break
+    elif age>20:
         credit_score=int(input("Enter your credit score: "))
-        if credit_score>580:
+        if 900>credit_score>580:
             income=int(input("Enter your yearly income : "))
             if income>10000:
 
@@ -31,11 +34,15 @@ while True:
                     print("Not approved: Insuffecient balance")
                     break
                 else:
-                    print("10%"+"of the income left will be stored for your free spending")
+                    print("10%"+" of the income left will be stored for your free spending")
                     spending=left_income%10
                     left_income-=spending
-    
+                    print("Money left for spending i.e (10%):",spending)
+
+                    print()
+
                     y=input("Do you have any previous loans[y/n]: ")
+                    print()
                     if y.lower()=='y':
                         num=int(input("Enter the number of pre existing loans: "))
                         if num<5:
@@ -51,6 +58,7 @@ while True:
                             not_used=(left_income/100)*25
                             left_income-=not_used
                             print("The money that will be alloted for all your loans= ",left_income)
+                            print()
                         else:
                             print("One cannot have more than 5 loans at a given point.")#just an example
                             break
@@ -68,6 +76,8 @@ while True:
             else:
                 print("Not approved: Due to income defficiencies.")
                 break
+        elif credit_score>900:
+            print("Score not possible ")
         else:
             print("Not approved:Due to insufficent credit score [more than 580]")
             break
